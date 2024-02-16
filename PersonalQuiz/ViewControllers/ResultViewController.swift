58b627887af0b0f54c2c1c763efd6d63e05d66e1
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-02-16 23:19:43 +0300

    Hiding the button back

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -11,7 +11,7 @@ final class ResultViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationItem.hidesBackButton = true
     }
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
